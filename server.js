1aaf6182710ab4e0dce290c95bb03fca2085d1bf
coderfromacts <107700152+coderfromacts@users.noreply.github.com>
2022-08-08 15:11:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h3>hello world</h3>");
+    res.send("<h3>Thanks for Party</h3>");
 });
 
 app.listen(7000);
